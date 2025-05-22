265cef3297222609b31d6912e0a7c9e97586be53
muradorucov <orucowmurad@gmail.com>
2025-05-22 21:34:55 +0400

    str

---
+++ Lesson-23/main.js
@@ -0,0 +1,224 @@
+// Dynamic => Js , Python , PHP
+// Static => C++, C, Java
+
+
+
+// let a = 10;
+// a = "salam"
+
+// Mobile (Js ReactNative) (Flutter Swift)
+// Gameing Threejs
+// Backend
+// FrontEnd
+// ML
+
+
+
+// TypeScript
+
+
+// Data types
+// Variables
+// INput&OUtput
+// Operators
+// Math
+// Compare
+// Condition
+// LOOP
+// Type convertions
+// Function ()
+// Arrays []
+// Objects {}
+// OOP
+
+
+// DOm
+// BOM
+// Event Loop
+// Async Programming
+
+
+
+// Interperator
+// Compiler
+
+
+// const a = 45
+// console.log(a)
+// a = "Salam";
+// console.log(a);
+// (async () => {
+//     throw new Error("Xeta Bash verdi");
+// })();
+// console.log("Neyi netice");
+
+
+// let a;
+// Primitve (value)
+// String=> "salam" 'hello' `hi` "56" "true" "[]"
+// Number=> 34 10 -9 0.6 20.5 | NaN(not a number) | Infinity
+// Boolean=> true or false
+// Null => null
+// Undefined => undefined
+// Symbol=> html elements
+// BigInt => 2**53
+
+// let a = BigInt(2**70)
+
+
+// Non-Primitive (referance)
+// Objects
+
+// Array,function,Map,Set,Date,Class,Object
+
+
+// "muradorucov"
+
+
+// Xetai ray,
+// Let
+// const
+// Var
+
+//      referance yeniden yaradila biler(unvan) | deyismek
+// const                olmaz                   | olmaz
+// let                  olmaz                   | olar
+// var                  olar                   | olar
+
+
+// const age = 80;
+// age = 70;
+
+// let age = 80;
+// age = 60
+
+
+// var age = 80;
+// console.log(age);
+
+// var age = 50;
+// console.log(age);
+
+
+// Hoisting
+// Scope({})
+
+// alert("Welcome to My Site")
+// let isConfirim = confirm("Yashiniz 25 -den boyukdur?")
+
+// if (isConfirim) {
+//     let username = prompt("Zehmet olmasa username daxil edin!", "anonim")
+//     document.writeln(`Welcome My Site ${username}`)
+// } else {
+//     document.writeln(`Girish yoxdur`)
+// }
+
+// console.log("test")
+// console.info("test")
+// console.warn("test")
+// throw new Error("Xetadir");
+
+// console.error("test")
+// console.warn("test")
+
+
+
+// Hosting
+// Hoisting
+// console.log(a);
+// a = 20;
+// let  a = 10;
+// test()
+// function test() {
+//     console.log("test");
+// }
+// var a;
+
+// a = 11;
+
+
+// TYPE CONVErTIONS
+// String | Number | Boolean
+
+// String to Number
+// String To Boolean
+// Number To String
+// Number To Boolean
+// Boolean To String
+// Boolean To NUmber
+// Null To NUmber
+// Null To boolean
+// Null To String
+// Undefined To Number
+// Undefined To boolean
+// Undefined To String
+
+// let str = "salam";
+// let val = Boolean(str)
+// let num = NaN;
+// let val = Boolean(num)
+// // let str = String(num)
+// console.log(val)
+
+// let val = false;
+// console.log(String(val))
+
+// console.log(Number(undefined))
+// console.log(String(undefined))
+// console.log(Boolean(undefined))
+
+
+
+// let _$fu_l$l28$_7Na$me2$3_ = "Test"
+
+// console.log("My name is Murad")
+// const name = "Murad";
+// console.log(name)
+
+// Depodaki bütün kitabların ümumi dəyərini konsola çıxarın. Kitab mağazasındaki kitabın qiyməti bookPrice dəyişənində və depodaki kitab sayı bookAmount dəyişənində saxlanılır.
+// let bookAmount = 75;
+// let bookPrice = 14;
+// let totalPrice = bookAmount * bookPrice
+// console.log(totalPrice + " AZN")
+// let kmhSpeed = 60;
+// let msSpeed = kmhSpeed * 1000 / 3600;
+// console.log(msSpeed)
+
+// let bdYear = +prompt("ZHmt olmasa dogum iliniz daxil edin!", 2000)
+// let age = 2025 - bdYear;
+// let days = age * 365
+// console.log(`Sizin yashadiginiz gunlerin sayi: ${days}`);
+
+// + - * / % **
+
+// console.log(10 / 12)
+
+// console.log(2 ** 54)
+// let num = 5
+
+// let val = Math.floor(num);
+// let val = Math.floor(num);
+// let val = Math.ceil(num);
+// let val = Math.round(num);
+// let val = Math.trunc(num);
+// let val = Math.round(Math.sqrt(num));
+// let val = Math.sqrt(num);
+
+// console.log(Math.pow(2, 4))
+// let num = Math.floor(Math.random() * 10)
+// 0.9434*10
+// console.log(num)
+
+// includes()
+// split()
+// toUpperCase()
+// toLowerCase()
+// indexOf()
+// lastIndexOf()
+// slice()
+// substring()
+// search()
+// startWith()
+// trim()
+// reaplace | ...all()
+
