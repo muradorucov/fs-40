2c87a95d5cd066e58382d4070287fa47f78039a3
muradorucov <orucowmurad@gmail.com>
2025-05-27 21:37:22 +0400

    feat:switch

---
+++ Lesson-25/main.js
@@ -0,0 +1,582 @@
+// Dynamic => Js , Python , PHP
+// Static => C++, C, Java
+
+
+
+// let a = 10;
+// a = "salam"
+
+// Mobile (Js ReactNative) (Flutter Swift)
+// Gameing Threejs
+// Backend
+// FrontEnd
+// ML
+
+
+
+// TypeScript
+
+
+// Data types
+// Variables
+// INput&OUtput
+// Operators
+// Math
+// Compare
+// Condition
+// LOOP
+// Type convertions
+// Function ()
+// Arrays []
+// Objects {}
+// OOP
+
+
+// DOm
+// BOM
+// Event Loop
+// Async Programming
+
+
+
+// Interperator
+// Compiler
+
+
+// const a = 45
+// console.log(a)
+// a = "Salam";
+// console.log(a);
+// (async () => {
+//     throw new Error("Xeta Bash verdi");
+// })();
+// console.log("Neyi netice");
+
+
+// let a;
+// Primitve (value)
+// String=> "salam" 'hello' `hi` "56" "true" "[]"
+// Number=> 34 10 -9 0.6 20.5 | NaN(not a number) | Infinity
+// Boolean=> true or false
+// Null => null
+// Undefined => undefined
+// Symbol=> html elements
+// BigInt => 2**53
+
+// let a = BigInt(2**70)
+
+
+// Non-Primitive (referance)
+// Objects
+
+// Array,function,Map,Set,Date,Class,Object
+
+
+// "muradorucov"
+
+
+// Xetai ray,
+// Let
+// const
+// Var
+
+//      referance yeniden yaradila biler(unvan) | deyismek
+// const                olmaz                   | olmaz
+// let                  olmaz                   | olar
+// var                  olar                   | olar
+
+
+// const age = 80;
+// age = 70;
+
+// let age = 80;
+// age = 60
+
+
+// var age = 80;
+// console.log(age);
+
+// var age = 50;
+// console.log(age);
+
+
+// Hoisting
+// Scope({})
+
+// alert("Welcome to My Site")
+// let isConfirim = confirm("Yashiniz 25 -den boyukdur?")
+
+// if (isConfirim) {
+//     let username = prompt("Zehmet olmasa username daxil edin!", "anonim")
+//     document.writeln(`Welcome My Site ${username}`)
+// } else {
+//     document.writeln(`Girish yoxdur`)
+// }
+
+// console.log("test")
+// console.info("test")
+// console.warn("test")
+// throw new Error("Xetadir");
+
+// console.error("test")
+// console.warn("test")
+
+
+
+// Hosting
+// Hoisting
+// console.log(a);
+// a = 20;
+// let  a = 10;
+// test()
+// function test() {
+//     console.log("test");
+// }
+// var a;
+
+// a = 11;
+
+
+// TYPE CONVErTIONS
+// String | Number | Boolean
+
+// String to Number
+// String To Boolean
+// Number To String
+// Number To Boolean
+// Boolean To String
+// Boolean To NUmber
+// Null To NUmber
+// Null To boolean
+// Null To String
+// Undefined To Number
+// Undefined To boolean
+// Undefined To String
+
+// let str = "salam";
+// let val = Boolean(str)
+// let num = NaN;
+// let val = Boolean(num)
+// // let str = String(num)
+// console.log(val)
+
+// let val = false;
+// console.log(String(val))
+
+// console.log(Number(undefined))
+// console.log(String(undefined))
+// console.log(Boolean(undefined))
+
+
+
+// let _$fu_l$l28$_7Na$me2$3_ = "Test"
+
+// console.log("My name is Murad")
+// const name = "Murad";
+// console.log(name)
+
+// Depodaki bütün kitabların ümumi dəyərini konsola çıxarın. Kitab mağazasındaki kitabın qiyməti bookPrice dəyişənində və depodaki kitab sayı bookAmount dəyişənində saxlanılır.
+// let bookAmount = 75;
+// let bookPrice = 14;
+// let totalPrice = bookAmount * bookPrice
+// console.log(totalPrice + " AZN")
+// let kmhSpeed = 60;
+// let msSpeed = kmhSpeed * 1000 / 3600;
+// console.log(msSpeed)
+
+// let bdYear = +prompt("ZHmt olmasa dogum iliniz daxil edin!", 2000)
+// let age = 2025 - bdYear;
+// let days = age * 365
+// console.log(`Sizin yashadiginiz gunlerin sayi: ${days}`);
+
+// + - * / % **
+
+// console.log(10 / 12)
+
+// console.log(2 ** 54)
+// let num = 5
+
+// let val = Math.floor(num);
+// let val = Math.floor(num);
+// let val = Math.ceil(num);
+// let val = Math.round(num);
+// let val = Math.trunc(num);
+// let val = Math.round(Math.sqrt(num));
+// let val = Math.sqrt(num);
+
+// console.log(Math.pow(2, 4))
+// let num = Math.floor(Math.random() * 10)
+// 0.9434*10
+// console.log(num)
+
+// includes()
+// split()
+// toUpperCase()
+// toLowerCase()
+// indexOf()
+// lastIndexOf()
+// slice()
+// substring()
+// search()
+// startWith()
+// trim()
+// replace | ...all()
+
+
+
+// null=>0
+// undefined=>NaN
+
+// console.log(Boolean('false'));
+// console.log(typeof null)
+
+// console.log("-9" * "a" + "b"); // -9 * NaN + "b" = NaN + "b" = "Nan" + "b" ="NaNb"
+
+
+//  + ve biri str => str  [ "4"  + 89 = "4" +"89"  ="489" ]
+// + ve hec biri str deyilse =>num [true + null = 1 + 0 = 1]
+//  + deyilse(* - /) her iki teref num olur =>num
+// console.log(NaN - "67" + 2 - NaN)
+// console.log("false" - "true") = NaN- NaN = NaN
+
+
+// console.log(7 / 0)
+
+
+// user = ""
+
+
+// a =a / 80
+// a += 80
+// a /= 80
+// a -= 80
+// a *= 80
+
+// a -= 7 + 10 / 8
+// a += 6
+// a -= 1
+
+// a++
+// ++a
+// --a
+// a--
+
+// let a = 10;
+// console.log(++a, a--)
+// console.log(a)
+
+// let a = 6; // 7 6
+// let b = a++ - --a + ++a// 6 - 6 + 7
+
+// console.log(a, b)
+// let a = 4;//0
+// let b = a + --a + a-- - --a; // 9 10
+// let c = b++ - a - --a; // 9 - 1 - 0 = 8
+// console.log(a, b, c)
+
+
+// let text = `Hello\`" 'World'`
+// console.log(text.length)
+// console.log(text[14])
+// text[0]
+// console.log(text)
+// index => 0
+// length => 1
+
+// let text = "Salam"
+// let username = "Admin";
+// // console.log(text + " " + username + "!")
+// console.log(`${text} ${username}!`)//template literals
+
+
+
+// let text = prompt("Zehmet olmasa adinizi daxil edin!");
+// if (text.includes("}")) {
+//     console.log("Adda Qadagan edilmish simvollar var")
+// } else {
+//     console.log("Xosh gelmisiniz!")
+// }
+
+
+// const fullName = prompt("");
+// const list = fullName.split(" ")
+// let firstName = list[0];
+// let lastName = list[1];
+// let fatherName = list[2]; //"Semed"
+
+// let result = `${lastName} ${firstName[0]}.${fatherName[0]}.`
+// console.log(result);
+
+
+
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+// let a = "15";
+// let b = "15";
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+
+
+// > < >= <= != !==  === ==
+
+
+// condition
+// if else
+
+
+// let a = -1;
+
+// if (a > 0) {
+//     console.log("A musbetdir")
+// }
+// else if (a < 0) {
+//     console.log("A menfidir")
+// }
+// else {
+//     console.log("A sifirdir")
+
+// };
+
+
+
+
+
+
+// if (a > 10) {
+//     console.log("A musbetdir")
+// }
+
+// if (a > 5) {
+//     console.log("A menfidir")
+// }
+
+// if (a === 0) {
+//     console.log("A sifirdir")
+// }
+
+
+
+
+
+// let answer = prompt("What is the “official” name of JavaScript");
+// if (answer === "ECMAScript") {
+//     alert("“Right!”")
+// } else {
+//     alert("You don’t know? ECMAScript!")
+// }
+
+// let userName = prompt("Who's there?");
+// if (userName === null || !userName.trim()) {
+//     alert("Canceled")
+// }
+// else if (userName !== "Admin") {
+//     alert("I don't know you")
+// }
+// else {
+//     let password = prompt("Password?");
+
+//     if (password === null || !password.trim()) {
+//         alert("Canceled")
+//     }
+//     else if (password !== "TheMaster") {
+//         alert("Wrong password")
+//     }
+//     else {
+//         alert("Welcome!")
+//     }
+// }
+
+
+
+//  45 46 47
+//  47 46 45
+
+// let nums = "45 46 47".split(" ");
+// let num1 = nums[0]
+// let num2 = nums[1]
+// let num3 = nums[2];
+
+// if ((num3 > num2 && num2 > num1) || (num2 > num3 && num2 < num1)) {
+//     console.log(num2)
+// }
+
+// console.log(1 && "salam" )
+// console.log()
+// if ("a" || true) {
+
+// }
+
+// let val = ""
+
+// console.log(false || true || false)
+
+
+// let value = 10;
+// console.log(value < 15)
+// console.log(value == 10)
+
+/*
+1."45 46 47"
+2."47 46 45"
+3."46 47 45"
+4."46 45 47"
+5."45 47 46"
+6."47 45 46"
+*/
+// const value = "10 41 4".split(" ");
+// let num1 = +value[0];
+// let num2 = +value[1];
+// let num3 = +value[2];
+
+// if (num2 > num1 && num2 < num3 || num2 < num1 && num2 > num3) {
+//     console.log(num2)
+// }
+// else if (num1 < num2 && num1 > num3 || num1 > num2 && num1 < num3) {
+//     console.log(num1)
+// }
+// else if (num3 > num1 && num3 < num2 || num3 < num1 && num3 > num2) {
+//     console.log(num3)
+// }
+// // else {
+// //     console.warn("Yanlish deger!")
+// // }
+
+
+
+// let weekDay = 43;
+// if (weekDay === 1) {
+//     console.log("Bazarertesi")
+// }
+// else if (weekDay === 2) {
+//     console.log("Chersenbe axshami");
+// }
+// else if (weekDay === 3) {
+//     console.log("Chersenbe");
+// }
+// else if (weekDay === 4) {
+//     console.log("Cume axshami");
+// }
+// else if (weekDay === 5) {
+//     console.log("Cume");
+// }
+// else if (weekDay === 6) {
+//     console.log("Shenbe");
+// }
+// else if (weekDay === 7) {
+//     console.log("Bazar");
+// }
+// else {
+//     console.warn("Yanlish deger!")
+// }
+
+
+
+
+
+
+// switch (weekDay) {
+//     case 1:
+//     case 2:
+//         console.log("Chersenbe axshami");
+//         break;
+//     case 3:
+//     case 4:
+//         console.log("Cume axshami");
+//         break;
+//     default:
+//         console.warn("Yanlish deger!")
+// }
+
+
+
+
+
+
+
+// let val = undefined;
+
+
+// val?.split("")
+
+
+let a = 10;
+
+// if (a > 0) {
+//     console.log("Musbetdir");
+// } else {
+//     console.log("Menfi ve ya sifirdir")
+// }
+
+// a > 0 ? console.log("Musbetdir")
+//     : a < 10 ? console.log("Menfidir")
+//         : a === 0 ? console.log("Sifirdir")
+//             : console.log("Yanlish deyer");
+
+
+// let c = 6;
+// let b = 7;
+// console.log)
+
+// c = b + c;//13
+// b = c - b;// 6 
+// c = c - b; // 7
+
+// [c, b] = [b, c]
+
+
+// let gender = "F";
+
+// let result = gender === "M" ? "Male" : gender === "F" ? "Female" : "Yanlish Deyer";
+
+// console.log(result);
+
+// let monthNumber = "10";
+// let result;
+// switch (monthNumber) {
+//     case "01":
+//     case "1":
+//         result = "Yanvar";
+//         break;
+//     case "10":
+//         result = "Oktyabr"
+//         break;
+//     case "11":
+//         result = "Noyabr";
+//         break;
+//     default:
+//         result = "Yanlish deyer"
+//         break;
+// }
+
+// console.log(result);
+
+
+// let date = "2009.1.05".split(".")
+// let year = date[0];
+// let month = date[1];
+// let day = date[2];
+// if (month <= 12 && month >= 1 && day >= 1 && day <= 30) {
+//     switch (month) {
+//         case "01":
+//         case "1":
+//             month = "Yanvar";
+//             break;
+//         case "02":
+//         case "2":
+//             month = "Fevral";
+//             break;
+//         case "03":
+//         case "3":
+//             month = "Mart";
+//             break;
+//     }
+//     console.log(`${day} ${month} ${year}-cu il`)
+// } else {
+//     console.log("Format Yanlishdir!");
+// }
+
