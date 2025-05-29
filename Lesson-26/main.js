09b0502ed5162b1f86a2b03dfa0e0386e31dcfd2
muradorucov <orucowmurad@gmail.com>
2025-05-29 20:48:38 +0400

    feat:loop

---
+++ Lesson-26/main.js
@@ -0,0 +1,85 @@
+// let a = 5,
+//     b = 3,
+//     op = "*"
+
+// let a = 10
+// console.log(a++);//10
+// console.log(++a);//12
+
+
+
+
+// for operators
+// while
+// // do While
+//  for of
+// for in
+
+
+// foreach methods
+
+
+// rekursive func
+
+
+
+
+// let count = 0
+// function customLoop() {
+//     if (count < 100) {
+//         console.log(count);
+//         count++
+//         return;
+//         customLoop()
+//     }
+// }
+
+// customLoop()
+
+
+// let a = 10
+// while (a > 0) {
+//     console.log(a);
+// }
+
+// if (a > 0) {
+//     console.log(a);
+// }
+
+
+// let fullName = "Murad Orucov";
+// let result = "Reqem yoxdur";
+
+// let i = 0;
+// while (i < fullName.length) {
+// if ((fullName[i] >= 0 || fullName[i] <= 9) && fullName[i] != " ") {
+//     result = "Reqem var"
+// }
+// if (Number(fullName[i]) || fullName[i] === "0") {
+//     result = "Reqem var"
+// }
+//     if (!isNaN(fullName[i]) && fullName[i] !== " ") {
+//         result = "Reqem var"
+//     }
+//     i++;
+// }
+
+// console.log(result);
+
+
+
+// let a = 0;
+// while (a < 10) {
+//     console.log(a);
+//     a++
+// }
+
+
+
+// for (let a = 0; a < 10; a++) {
+//     console.log(a);
+// }
+
+
+
+// "12321" "ata" "ana" ""
