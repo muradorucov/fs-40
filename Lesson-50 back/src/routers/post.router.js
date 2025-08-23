b01ea0709b70c246e121a2e1bb2fa6f986cfc475
muradorucov <orucowmurad@gmail.com>
2025-08-23 20:13:34 +0400

    feat:users done

---
+++ Lesson-50 back/src/routers/post.router.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const postRouter = express.Router();
+
+// postRouter.get("/")
+// postRouter.post("/")
+// postRouter.put("/:id")
+// postRouter.get("/:id")
+// postRouter.delete("/:id")
+
+
+// module.exports = postRouter
